bb95f901946ab7c021ed56950723f7b6504ba5d3
mukul <mukulraw199517@gmail.com>
2021-02-15 08:21:32 +0530

    UI changes Done

--- app/src/main/java/com/app/onnwaytransporter/SelectUserType.java
+++ app/src/main/java/com/app/onnwaytransporter/SelectUserType.java
@@ -47,7 +47,7 @@ public class SelectUserType extends AppCompatActivity {
         truckDriver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SelectUserType.this, Profile.class);
+                Intent intent = new Intent(SelectUserType.this, Profile2.class);
                 intent.putExtra("user" , "driver");
                 startActivity(intent);
             }
